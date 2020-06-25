e69dfe97241615038eb4815a80cf9e28afc362d1
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-06-25 13:06:31 +0000

    Function random username gen with buttons that determine if they have one

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, flash
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -6,10 +6,6 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI']=getenv('FLASK_BLOG_URI')
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
 
