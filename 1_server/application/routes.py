fb06d672a1cd0b2a3e1c0013ca1df397b12976d7
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-07-06 08:56:32 +0000

    SMall edit to server routes, but in wrong directory

--- 1_server/application/routes.py
+++ 1_server/application/routes.py
@@ -1,31 +1,21 @@
 from application import app
-from flask import render_template, redirect, url_for, request, flash
-import string, random
+from flask import render_template, redirect, url_for, request
+from application.forms import GenerateTrickButton
+import requests
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    username = ''
-    strings = ''.join(random.choices(string.ascii_lowercase[0:3], k=5))
-    numbers = ''.join(random.choices(string.digits, k=3))
-    prize =''
-    for i in range(len(strings)):
-        if i+1 > len(numbers):
-            username += strings[i]
-        else:
-            username += strings[i] + numbers[i]
 
-    if username[0]=='a':
-        if random.randrange(1,100,1)<=25:
-            prize= "Youve won £100!"
-        elif random.randrange(1,100,1)<=75:
-            prize='Youve won £50!'
-        else:
-            prize="You didn't win this time"
-    else:
-        prize='Youve lost'
-    
-    return render_template('home.html', title='Home', username=username, prize=prize)
+    generate_trick = GenerateTrickButton()
 
+    if request.method == "GET":
+        full_trick = {
+                "stance" : "",
+                "trick" : ""
+                }
 
+        if generate_trick.is_submitted():
+            response = requests.get("http://SKATE:5000/service4/get_package")
+            full_trick = response.json()
