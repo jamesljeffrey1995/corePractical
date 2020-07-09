d9f29c6299d08def1e5d2e9b2e102182af250faa
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-07-09 19:38:39 +0000

    Swarm, nginx can connect from vm

--- 2_stance/application/routes.py
+++ 2_stance/application/routes.py
@@ -1,15 +1,17 @@
+from application.models import Stance
 from flask import render_template, redirect, url_for, request, jsonify
-from application import app
+from application import app, db
 import random
-import requests 
+import requests
 import sqlalchemy
 
 
 @app.route('/service2/get_package', methods = ["GET", "POST"])
 def generate_stance():
-    stances = {1:"Normal", 2:"Switch", 3:"Nollie", 4:"Fakie"}
-    app.logger.info("Database fetched succesful")
-    response = stances[random.randint(1,4)]
-    stance = { "stance" : response }
-    app.logger.info(stance) 
+    app.logger.info("Recieved")
+    randomnum = random.randint(1,4)
+    query = Stance.query.filter_by(id=randomnum).first()
+    app.logger.info(query.stance)
+    stance = query.stance
+    stance = { "stance" : stance }
     return jsonify(stance)
