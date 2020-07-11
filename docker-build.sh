0709b2d42cba0ef9942d4c287a02dacb30aeebe6
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-07-11 11:16:23 +0000

    Hopeful fix

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 docker-compose build
 docker-compose push
-scp .env
+scp .env corepractical:/home/jamesljeffrey1995/corePractical/.env
 scp docker-compose.yaml corepractical:/home/jamesljeffrey1995/corePractical/docker-compose.yaml
-ssh corepractical << EOF
+ssh -i c~/.ssh/ansible_id_rsa corepractical << EOF
 cd /home/jamesljeffrey1995/corePractical
 env MY_SECRET_KEY=${MY_SECRET_KEY} env USERNAME=${USERNAME} env PASSWORD=${PASSWORD} env IP=${IP} env DATABASE=${DATABASE}  docker stack deploy --compose-file docker-compose.yaml corePractical
 EOF
