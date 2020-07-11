a91cb5cf27949199bb13dc3710e05ddff3ace041
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-07-11 08:46:20 +0000

    Hopefully work with jenkins

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 docker-compose build
-docker-compose push 
-ssh corepractical << EOF
-cd /home/jamesljeffrey1995/corePractical/
+docker-compose push
+scp docker-compose.yaml jenkins@corepractical:/home/jamesljeffrey1995/corePractical/docker-compose.yaml
+ssh jenkins@corepractical << EOF
+cd /home/jamesljeffrey1995/corePractical
 env MY_SECRET_KEY=${MY_SECRET_KEY} env USERNAME=${USERNAME} env PASSWORD=${PASSWORD} env IP=${IP} env DATABASE=${DATABASE}  docker stack deploy --compose-file docker-compose.yaml corePractical
 EOF
