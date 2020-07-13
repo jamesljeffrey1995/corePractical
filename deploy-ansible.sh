e318b3d24d1d0ff71031f2bf02ce833c950bdb28
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-07-13 06:17:09 +0000

    nginx

--- deploy-ansible.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-/home/jenkins/.local/bin/ansible-playbook -i inventory playbook.yaml
--- docker-build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-docker-compose build
-docker-compose push
-scp .env corepractical:/home/jamesljeffrey1995/corePractical/.env
-scp docker-compose.yaml corepractical:/home/jamesljeffrey1995/corePractical/docker-compose.yaml
-ssh -i ~/.ssh/ansible_id_rsa corepractical << EOF
-cd /home/jamesljeffrey1995/corePractical
-env MY_SECRET_KEY=${MY_SECRET_KEY} env USERNAME=${USERNAME} env PASSWORD=${PASSWORD} env IP=${IP} env DATABASE=${DATABASE}  docker stack deploy --compose-file docker-compose.yaml corePractical
-EOF
