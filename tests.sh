02fb67c7a7fa5e1dd5dada0c7cdf910865986790
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-07-13 03:35:35 +0000

    auto Test

--- tests.sh
+++ tests.sh
@@ -7,7 +7,7 @@ ssh -i ~/.ssh/ansible_id_rsa corepractical << EOF
 
 
 
-. /home/jamesljeffrey1995/corePractical/venv/bin/activate
+. /home/jamesljeffrey1995/venv/bin/activate
 
 cd /home/jamesljeffrey1995/corePractical/1_server/Testing
 pytest --cov ~/corePractical/1_server/application
