85ed90fcc0f88add2640603391851120c3f66cd9
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-07-13 01:33:41 +0000

    auto Test

--- tests.sh
+++ tests.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
+docker-compose build
+docker-compose push
+scp -i ~/.ssh/ansible_id_rsa .env corepractical:/home/jamesljeffrey1995/corePractical/.env
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml corepractical:/home/jamesljeffrey1995/corePractical/docker-compose.yaml
+ssh -i ~/.ssh/ansible_id_rsa corepractical << EOF
 
 
 
-
-. /home/jamesljeffrey1995/corePractical/venv/bin/activate
+. /home/jamesljeffrey1995/venv/bin/activate
 
 cd /home/jenkins/.jenkins/workspace/corePractical/1_server
 pytest --cov ./application
